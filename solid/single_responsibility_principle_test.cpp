f90b020e732f2a3bd7fe4476c7622f5b99a3c11f
Franek Borowiec <franek.borowiec@gmail.com>
2020-05-14 22:04:15 +0200

    Single Responsibility Principle file structure

---
+++ solid/single_responsibility_principle_test.cpp
@@ -0,0 +1,13 @@
+#include "single_responsibility_principle.hpp"
+
+#include "gtest/gtest.h"
+
+namespace
+{
+
+TEST(SingleResponsiblityPrincipleTest, Dummy_Pass)
+{
+  ASSERT_TRUE(1);
+}
+
+}  // namespace
