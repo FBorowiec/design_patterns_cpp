3736a969737f9c164fdf40f8619909e0b0af273b
Franek Borowiec <franek.borowiec@gmail.com>
2020-05-25 21:51:54 +0200

    Composite pattern example on neural network

---
+++ structural_patterns/composite_pattern/composite_pattern.cpp
@@ -0,0 +1,71 @@
+/**
+ * Treating individual and aggregate objects uniformly.
+ * MOTIVATION:
+ * Objects use other objects' fileds/properties/members through inheritance and composition.
+ * Composition lets us make compound objects:
+ *  - e.g, a mathematical expression composed of simple expressions
+ *  - a grouping of shapes that consists of several shapes
+ * Composite desing pattern is used to treat both single (scalar) and composite objects uniformly:
+ *  - i.e. Foo and Collection<Foo> have common APIs
+ */
+#include<iostream>
+#include<string>
+#include<vector>
+
+namespace structural
+{
+namespace composite_pattern
+{
+
+// Composite
+struct GraphicObject
+{
+  virtual void Draw() = 0;
+};
+
+struct Circle : public GraphicObject
+{
+  void Draw() override { std::cout<< "Circle" << std::endl; }
+};
+
+struct Group : GraphicObject
+{
+  Group(std::string name) : name_(name) {}
+
+  void Draw() override
+  {
+    std::cout << "Group " << name_ << " contains: " << std::endl;
+    for (auto&& o : objects)
+      o->Draw();
+  }
+
+  std::string name_;
+  std::vector<GraphicObject*> objects;
+};
+
+}  // namespace composite_pattern
+}  // namespace structural
+
+// TEST---------------------------------------------------------------------------------------------------------------|
+
+#include "gtest/gtest.h"
+
+namespace
+{
+
+using namespace structural::composite_pattern;
+
+TEST(CompositePatternTest, UsageOfTheCompositePattern)
+{
+  Group root("root");
+  Circle c1, c2;
+  root.objects.push_back(&c1);
+
+  Group subgroup("sub");
+  subgroup.objects.push_back(&c2);
+  root.objects.push_back(&subgroup);
+
+  root.Draw();
+}
+
+}  // namespace
---
+++ structural_patterns/composite_pattern/neural_network.cpp
@@ -0,0 +1,112 @@
+#include <iostream>
+#include <vector>
+
+namespace structural
+{
+namespace composite_pattern
+{
+
+struct Neuron;
+
+// CRTP - Curiously Recurring Template Pattern
+template <typename Self>
+struct SomeNeurons
+{
+  template <typename T> void ConnectTo(T& other)
+  {
+    for (Neuron& from : *static_cast<Self*>(this))
+    {
+      for (Neuron& to : other)
+      {
+        from.out.push_back(&to);
+        to.in.push_back(&from);
+      }
+    }
+  }
+};
+
+struct Neuron : public SomeNeurons<Neuron>
+{
+  std::vector<Neuron*> in, out;
+  unsigned int id;
+
+  Neuron()
+  {
+    static unsigned int id_{1};
+    this->id = id_++;
+  }
+
+  Neuron* begin() { return this; }
+  Neuron* end() { return this+1; }
+
+  void ConnectTo(Neuron& other)
+  {
+    out.push_back(&other);
+    other.in.push_back(this);
+  }
+
+  friend std::ostream& operator<<(std::ostream& os, const Neuron& obj)
+  {
+    for (Neuron* n : obj.in)
+      os << " " << n->id << " " << "\t-->\t[" << obj.id << "]" << std::endl;
+    for (Neuron* n : obj.out)
+      os << "[" << obj.id << "]\t-->\t" << " " << n->id << " " <<std::endl;
+    return os;
+  }
+};
+
+// Inheriting from stl standard types is not recommended as they do not have virtual destructors
+struct NeuronLayer : public std::vector<Neuron>, SomeNeurons<NeuronLayer>
+{
+  NeuronLayer(int count)
+  {
+    while(count --> 0)
+      emplace_back(Neuron{});
+  }
+
+  friend std::ostream& operator<<(std::ostream& os, const NeuronLayer& obj)
+  {
+    for (const auto& n : obj)
+      os << n;
+    return os;
+  }
+};
+
+}  // namespace composite_pattern
+}  // namespace structural
+
+// TEST---------------------------------------------------------------------------------------------------------------|
+
+#include "gtest/gtest.h"
+
+namespace
+{
+
+using namespace structural::composite_pattern;
+
+TEST(CompositePatternNNTest, UsageOfTheCompositePattern)
+{
+  Neuron n1, n2;
+  n1.ConnectTo(n2);
+  std::cout << n1 << n2 << std::endl;
+}
+
+TEST(CompositePatternNNTest, UsageOfTheCompositePatternForNeuronLayers)
+{
+  Neuron n1, n2;
+  n1.ConnectTo(n2);
+
+  NeuronLayer layer1{5};
+  Neuron n3;
+  layer1.ConnectTo(n3);
+
+  std::cout << "Neuron " << n3.id << std::endl << n3 <<std::endl;
+
+  NeuronLayer layer2{2}, layer3{3};
+  layer2.ConnectTo(layer3);
+
+  std::cout << "Layer 2 " <<std::endl << layer2;
+  std::cout << "Layer 3 " <<std::endl << layer3;
+}
+
+}  // namespace
