dce7d63fac807873c1a093c4d6c2572aaa456e2b
Franek Borowiec <franek.borowiec@gmail.com>
2020-05-27 22:35:25 +0200

    Property proxy pattern

---
+++ structural_patterns/proxy_pattern/property_proxy_pattern.cpp
@@ -0,0 +1,53 @@
+/**
+ * Acts as the "getter and setter" for a particular field
+ */
+#include <iostream>
+
+namespace structural
+{
+namespace proxy_pattern
+{
+
+template<typename T> struct Property
+{
+  T value_;
+
+  Property(T value)
+  {
+    *this = value;
+  }
+
+  T operator=(T new_value)
+  {
+    std::cout << "Assignment!\n";
+    return value_ = new_value;
+  }
+};
+
+struct Creature
+{
+  Property<int> strenght{10};
+  Property<int> agility{10};
+};
+
+}  // namespace proxy_pattern
+}  // namespace structural
+
+// TEST---------------------------------------------------------------------------------------------------------------|
+
+#include "gtest/gtest.h"
+
+namespace
+{
+
+using namespace structural::proxy_pattern;
+
+TEST(PropertyProxyPatternTest, UsageOfThePropertyProxyPattern)
+{
+  Creature c;
+  c.strenght = 11;
+  auto& x = c.agility;
+  x = 3;
+}
+
+}  // namespace
