1bbfbc87ac9a1ccb7a44a5cc28cf17628351952b
Franek Borowiec <franek.borowiec@gmail.com>
2020-06-23 22:06:52 +0200

    Observer and Iterator patterns

---
+++ behavioral_patterns/iterator_pattern/iterator_pattern.cpp
@@ -0,0 +1,62 @@
+/**
+ * How traversal of data structures happens and who makes it happen.
+ * MOTIVATION:
+ * Iteration (traversal) is a core functionality of various data.
+ * An iterator is a class that facilitates the traversal:
+ *  - Keeps a reference to the current element
+ *  - Knows how to move to a different element
+ * Can be used implicitely:
+ *  - Range-based for loop
+ *  - Coroutines
+ *
+ * The iterator is an object that facilitates the traversal of a data structure.
+ */
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "gtest/gtest.h"
+
+namespace behavioral {
+namespace iterator_pattern {
+namespace {
+
+TEST(IteratorPatternTest, IteratorsInTheStandardTemplateLibrary) {
+  std::vector<std::string> names{"John", "Jane", "Jill", "Jack"};
+
+  // iterator over vector of strings
+  std::vector<std::string>::iterator it = names.begin();  // auto
+  std::cout << "First name is: " << *it << std::endl;
+
+  ++it;
+  it->append(std::string(" Goodall"));
+  std::cout << "Second name is: " << *it << std::endl;
+
+  while (++it != names.end()) {
+    std::cout << "Another name: " << *it << std::endl;
+  }
+
+  // reverse iterator over vector of strings
+  for (auto ri = std::rbegin(names); ri != rend(names); ++ri) {
+    std::cout << *ri;
+    if (ri+1 != std::rend(names)) std::cout << ", ";
+  }
+  std::cout << std::endl;
+
+  // const reverse iterator over vector of strings
+  std::vector<std::string>::const_reverse_iterator jack = std::crbegin(names);
+  // *jack += std::string(" Reacher"); <-- FAILS!
+
+  /**
+   * Ranged-based for-loop:
+   * begin/end
+   * *it
+   */
+  for (auto&& name : names) {
+    std::cout << "Name: " << name << std::endl;
+  }
+}
+
+}  // namespace
+}  // namespace iterator_pattern
+}  // namespace behavioral
---
+++ behavioral_patterns/observer_pattern/observer_pattern.cpp
@@ -0,0 +1,97 @@
+/**
+ * Getting notification when things happen.
+ * MOTIVATION:
+ * We need to be informed when certain things happen:
+ *  - Object's field changes
+ *  - Object does something
+ *  - Some external event occurs
+ * We want to listen to events and notified when they occur:
+ * Terminology:
+ *  - event and subscriber
+ *  - signal and slots
+ *
+ * An observer is an object that wishes to be informed about events happening in the system.
+ * The entity generating the events is an observable
+ */
+#include <iostream>
+#include <string>
+#include <vector>
+
+namespace behavioral {
+namespace observer_pattern {
+
+template <typename>
+class Observer;
+
+template <typename T>
+class Observable {
+ public:
+  void Notify(T& source, const std::string& field_name) {
+    for (auto o : observers) {
+      o->FieldChange(source, field_name);
+    }
+  }
+
+  void Subscribe(Observer<T>& observer) { observers.push_back(&observer); }
+
+  void Unubscribe(Observer<T>& observer) {
+    observers.erase(remove(observers.begin(), observers.end(), &observer), observers.end());
+  }
+
+  std::vector<Observer<T>*> observers;
+};
+
+class Person : public Observable<Person> {
+ public:
+  Person(int age) : age_(age) {}
+
+  void SetAge(int age) {
+    if (this->age_ == age) return;
+    this->age_ = age;
+    Notify(*this, "age");
+  }
+
+  int GetAge() { return age_; }
+
+ private:
+  int age_;
+};
+
+// observer & observable
+template <typename T>
+class Observer {
+ public:
+  virtual void FieldChange(T& source, const std::string& field_name) = 0;
+};
+
+struct ConsolePersonObserver : public Observer<Person> {
+  virtual void FieldChange(Person& source, const std::string& field_name) override {
+    std::cout << "Person's " << field_name << " has changed to";
+    if (field_name == "age") std::cout << source.GetAge() << std::endl;
+  }
+};
+
+}  // namespace observer_pattern
+}  // namespace behavioral
+
+// TEST---------------------------------------------------------------------------------------------------------------|
+
+#include "gtest/gtest.h"
+
+namespace {
+
+using namespace behavioral::observer_pattern;
+
+TEST(ObserverPatternTest, UsageOfTheObserverPattern) {
+  Person person{10};
+  ConsolePersonObserver cpo;
+  person.Subscribe(cpo);
+
+  person.SetAge(11);
+  person.SetAge(12);
+
+  person.Unubscribe(cpo);
+  person.SetAge(13);
+}
+
+}  // namespace
