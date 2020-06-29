11c501d5445881ac1a33b41ee5712bdfcaabb956
Franek Borowiec <franek.borowiec@gmail.com>
2020-06-29 20:49:19 +0200

    State pattern skeleton

---
+++ behavioral_patterns/state_pattern/state_pattern.cpp
@@ -0,0 +1,28 @@
+/**
+ * INTENT:
+ * Finite state machines.
+ * MOTIVATION:
+ *
+ */
+
+namespace behavioral {
+namespace state_pattern {
+
+
+
+}  // namespace state_pattern
+}  // namespace behavioral
+
+// TEST---------------------------------------------------------------------------------------------------------------|
+
+#include "gtest/gtest.h"
+
+namespace {
+
+using namespace behavioral::state_pattern;
+
+TEST(StatePatternTest, UsageOfTheStatePattern) {
+
+}
+
+}  // namespace
