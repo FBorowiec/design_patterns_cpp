6763dabb2b95a360cd6ba66f3960c6a05cccea3c
Franek Borowiec <franek.borowiec@gmail.com>
2020-07-12 17:37:38 +0200

    Reflective visitor pattern

---
+++ behavioral_patterns/visitor_pattern/reflective_visitor_pattern.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <sstream>
+
+namespace behavioral {
+namespace visitor_pattern {
+
+
+
+}  // namespace visitor_pattern
+}  // namespace behavioral
+
+// TEST---------------------------------------------------------------------------------------------------------------|
+
+#include "gtest/gtest.h"
+
+namespace {
+
+using namespace behavioral::visitor_pattern;
+
+TEST(VisitorPatternTest, UsageOfTheReflectiveVisitorPattern) {
+  
+}
+
+}  // namespace
