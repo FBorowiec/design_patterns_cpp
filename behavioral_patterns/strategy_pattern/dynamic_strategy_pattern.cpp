c8c31a2ad05a9f4c79438a9d85b01ad5e5c199db
Franek Borowiec <franek.borowiec@gmail.com>
2020-07-12 02:02:34 +0200

    Complete strategy pattern wiki

--- behavioral_patterns/strategy_pattern/dynamic_strategy_pattern.cpp
+++ behavioral_patterns/strategy_pattern/dynamic_strategy_pattern.cpp
@@ -11,6 +11,11 @@
  * The strategy design pattern enables the exact behavior of a system to be selected
  * either at run-time (dynamic) or compile-time (static).
  * Also know as the Policy design patterns.
+ *
+ * Steps:
+ *  - Define an algorithm at a high level
+ *  - Define the interface you expect each strategy to follow
+ *  - Provide for either dynamic or static composition of strategy in the overall algorithm
  */
 #include <iostream>
 #include <memory>
