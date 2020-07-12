463f437dbecf8f615fb6c36ddb5366317cd4e27d
Franek Borowiec <franek.borowiec@gmail.com>
2020-07-12 02:32:27 +0200

    Intrusive visitor pattern

--- behavioral_patterns/visitor_pattern/intrusive_visitor_pattern.cpp
+++ behavioral_patterns/visitor_pattern/intrusive_visitor_pattern.cpp
@@ -14,11 +14,52 @@
  * Implemented by propagating a single visit() method throught the entire hierarchy.
  */
 #include <iostream>
+#include <sstream>
 
 namespace behavioral {
 namespace visitor_pattern {
 
+struct Expression {
+  virtual ~Expression() = default;
+  virtual void Print(std::ostringstream& oss) = 0;
+};
 
+struct DoubleExpression : public Expression {
+  DoubleExpression(double value) : value_(value) {}
+  virtual void Print(std::ostringstream& oss) override { oss << value_; }
+
+ private:
+  double value_;
+};
+
+struct AdditionExpression : public Expression {
+  /**
+   * 1+2+3
+   *    +
+   *   / \
+   *  1   +
+   *     / \
+   *    2   3
+   */
+ public:
+  AdditionExpression(Expression* left, Expression* right) : left_(left), right_(right) {}
+  ~AdditionExpression() {
+    delete left_;
+    delete right_;
+  }
+
+  virtual void Print(std::ostringstream& oss) override {
+    oss << "(";
+    left_->Print(oss);
+    oss << "+";
+    right_->Print(oss);
+    oss << ")";
+  }
+
+ private:
+  Expression* left_;
+  Expression* right_;
+};
 
 }  // namespace visitor_pattern
 }  // namespace behavioral
@@ -32,6 +73,15 @@ namespace {
 using namespace behavioral::visitor_pattern;
 
 TEST(VisitorPatternTest, UsageOfTheVisitorPattern) {
+  auto e = new AdditionExpression{new DoubleExpression{1},
+                                  new AdditionExpression{new DoubleExpression{2}, new DoubleExpression{3}}};
+
+  // how to print (1+(2+3))?
+  std::ostringstream oss;
+  e->Print(oss);
+  std::cout << oss.str() << std::endl;
+
+  delete e;
 }
 
 }  // namespace
